f8d8aaac11a6087484de6bcc90eb9ae669dc6173
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-07-20 21:17:25 -0400

    Update

--- api/server.js
+++ api/server.js
@@ -1,18 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const router = require('express').Router();
+const bcrypt = require("bcryptjs")
+const users = require("../users/users-model")
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
 
-const server = express();
+router.post('/register', async (req, res, next) => {
+  try {
+    const {username} = req.body
+    const user = await users.findBy({username}).first()
 
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+    if(user){
+        return res.status(409).json({
+            message: "username already taken"
+        })
+    }
+    res.status(201).json(await users.add(req.body))
+}catch(err){
+    next(err)
+}
+});
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+  users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compare(password, user.password)) {
+        console.log(req.session)
+        req.session.user = user;
+        res.status(200).json({ message: `Welcome back ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-module.exports = server;
+module.exports = router;
